ff838799a774a835152b025dc6d43f8dab8813b0
Svhodro <rudrolipi@gmail.com>
2024-06-24 01:13:14 +0600

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="absolute inset-0">
           <img
             src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw2fHxjb2RlfGVufDB8MHx8fDE2OTQwOTg0MTZ8MA&ixlib=rb-4.0.3&q=80&w=1080"
-            alt=" Image"
+            alt="Image"
             className="object-cover object-center w-full h-full"
           />
           <div className="absolute inset-0 bg-black opacity-50" />
